685076f471721558e5c80686310273b9e9b6c6eb
Dalton Scharff <dalton.scharff@gmail.com>
2021-12-23 09:19:15 -0600

    implements store with getItems function

---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import config from "../config.json";
+
+const firebaseConfig = config.firebase;
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export type Item = {
+  type: string;
+  name: string;
+  lowPrice: number;
+  highPrice: number;
+};
+
+export async function getItems(): Promise<Item[]> {
+  const itemsCol = collection(db, "items");
+  const itemSnapshot = await getDocs(itemsCol);
+  const itemList = itemSnapshot.docs.map((doc) => doc.data() as Item);
+  return itemList;
+}
+
+getItems().then((items: Item[]) => console.log(items));
