2657e89a6b79e22539e55f8eb38ad972bf4a0114
Dalton Scharff <dalton.scharff@gmail.com>
2021-12-24 08:34:03 -0600

    styles single item component

--- src/components/SingleItem.tsx
+++ src/components/SingleItem.tsx
@@ -17,13 +17,15 @@ function displayCurrency(price: number): string {
 function SingleItem({ item, onSelect, selected }: Props) {
   return (
     <div
+      className={`p-8 text-center border-2 rounded-lg box-shadow ${
+        selected ? "border-sky-500" : "border-slate-200"
+      }`}
       onClick={() => {
         onSelect(item);
       }}
     >
-      <h1>{item.name}</h1>
-      <p>selected: {selected.toString()}</p>
-      <p>
+      <h1 className="text-2xl font-bold">{item.name}</h1>
+      <p className="pt-2">
         {displayCurrency(item.lowPrice)} - {displayCurrency(item.highPrice)}
       </p>
     </div>
